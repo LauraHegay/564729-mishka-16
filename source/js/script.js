1eaa9623fe707e32926bbc263b46a1c9d69386ec
LauraHegay <sashanika79@gmail.com>
2019-05-27 12:13:37 +0300

    Исправление файла script.scss

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,23 @@
+// Показ модального окна
+var link = document.querySelector(".week-product__order");
+var overlay = document.querySelector(".page-overlay");
+var popup = document.querySelector(".page-modal");
+var close = popup.querySelector(".button--close");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("page-modal--show");
+  overlay.classList.add("page-overlay--show");
+});
+
+close.addEventListener("click", function (evt) {
+  // evt.preventDefault();
+  popup.classList.remove("page-modal--show");
+  overlay.classList.remove("page-overlay--show");
+});
+
+// открытие-закрытие меню
+
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
